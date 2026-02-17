b8bffaac792ad2e404e7b856641e05bbf6737aeb
RAGEJAX <109853879+ragej4x@users.noreply.github.com>
2026-02-17 20:45:40 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -305,5 +305,6 @@ def health_check():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=9000)
+        app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
